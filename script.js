b4c3cf5c7b674c0660b4a5b737b0a4f059e4254d
Amine KOUIS <amine.kouis@gmail.com>
2024-01-06 19:44:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+    function apendmmr(){
+    $("#outbklresult").val('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Google backlink maker for by mr laboratory </title></head><body><h1> Rediect backlink for <a href="'+$("#bkgurl").val()+'">'+$("#bkgurl").val()+'</a> by <a href="https://www.example.com/">Example</a></h1>'+mrmakeblinkmr($("#bkgurl").val())+mritem10+'<p>Rediect backlink for <a href="'+$("#bkgurl").val()+'">'+$("#bkgurl").val()+'</a> by <a href="https://www.mrlaboratory.info/">MR Laboratory</a></p> </body></html>');}
+     function mrdataapnd(d){$(".tabledata").append(d); };
+     function mrmakeblinkmr(u){
+         var r='<a target="_blank" href="https://www.fudbal91.com/tz.php?zone=America/Iqaluit&amp;r=' + u + ' "> https://www.fudbal91.com/tz.php?zone=America/Iqaluit&amp;r=' + u + ' </a> <a target="_blank" href="https://edusearch.ir/Goto.aspx?url=' + u + ' "> https://edusearch.ir/Goto.aspx?url=' + u + ' </a>';
+         return r; }
+    
+     $(".divoutbklresult .copyoutput").on("click",function(){ navigator.clipboard.writeText($(".divoutbklresult textarea").val()); 
+    $(".divoutbklresult .copyoutput").html("Copyed");
+    })
+           
+
+
+    //  for(var i = 0; i < $(".mrrrm a").length ; i++) {
+    //     (function(index) {
+    //         setTimeout(function() { 
+    // var u = $(".mrrrm a:eq("+index+")").attr('href');
+    // var popupwin = window.open(u,'anyname','width=300,height=300,right=10,bottom=10');
+    // $(".tabledata2").append("<a href='"+u+"'>"+u+"</a></br>");
+    // setTimeout(function() { popupwin.close();}, 2000);
+    //         }, index*2000);
+    //     })(i);
+    // }
